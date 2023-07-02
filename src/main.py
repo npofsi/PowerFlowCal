ad239bdae67f06676b014833bb308ce35abe0ab6
npofsi | N/P硅 <npofsi@outlook.com>
2023-07-02 10:38:02 +0800

    Refactor: add Components

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import os
-from powerflow.model import Profile, NodeType
+from powerflow.model import Model, Profile, NodeType
 root = os.getcwd()
 
 
 if __name__ == '__main__':
-    print(Profile(os.path.join(root,"tests\\IEEE-14.th")))
+    profile=Profile(os.path.join(root,"tests\\IEEE-14.th"))
+    print(profile)
+    model = Model()
+    model.compose(profile)
+
+
 
     
--- src/powerflow/model.py
+++ src/powerflow/model.py
@@ -3,6 +3,7 @@ from typing import Any
 import numpy as np
 from enum import Enum
 
+from powerflow.component import Component
 
 class NodeType(Enum):
     PQ = 1
@@ -18,10 +19,12 @@ class Node:
         self.P = P
         self.Q = Q
         self.V = V
-        self.Smin = 0
-        self.Smax = 100
-        self.Ymin = 0+0j
-        self.Ymax = 100+100j
+        self.Pmin = 0
+        self.Pmax = 100
+        self.Qmin = 0
+        self.Qmax = 100
+        self.Vmin = 0
+        self.Vmax = 100
         self.Ys = Ys
         self.theta = theta
         self.connectedBranches = []
@@ -37,12 +40,11 @@ class Node:
         self.connectedBranches.append(branch)
 
     def __str__(self) -> str:
-        print(
-            f'Node {self.name}:\n\tType: {self.type}\n\tP: {self.P}\n\tQ: {self.Q}\n\tV: {self.V}\n\ttheta: {self.theta}')
+        return f'Node {self.name}:\n\tType: {self.type}\n\tP: {self.P}\n\tQ: {self.Q}\n\tV: {self.V}\n\ttheta: {self.theta}'
 
 
 class Branch:
-    def __init__(self, name, type, node1, node2, Y=0+0j):
+    def __init__(self, name, type, node1: Node, node2: Node, Y=0+0j):
         self.name = name
         self.type = type
         self.node1 = node1
@@ -52,7 +54,7 @@ class Branch:
         self.Y = Y
 
     def __str__(self) -> str:
-        print(f'Branch {self.name}:\n\tNode1: {self.node1.name}\n\tNode2: {self.node2.name}\n\tR: {self.R}\n\tX: {self.X}\n\tB: {self.B}')
+        return f'Branch {self.name}:\n\tNode1: {self.node1.name}\n\tNode2: {self.node2.name}\n\tR: {self.R}\n\tX: {self.X}\n\tB: {self.B}'
 
 
 class Profile:
@@ -65,15 +67,21 @@ class Profile:
         self.data = [line for line in self.data if line[0][0] != '*']
 
     def __str__(self) -> str:
+        result = ''
         for line in self.data:
-            print(' '.join(line))
+            result += ' '.join(line)+'\n'
+        return result
 
 
 class Model:
     def __init__(self):
-        self.groundNode = Node('GND', NodeType.PV, P=0, V=0)
-        self.nodes = [self.groundNode]
+        self.nodes = []
         self.branches = []
+
+    def compose(self, profile: Profile):
+        self.profile = profile
+        self.componentManager = ComponentManager()
+        self.componentManager.parseProfile(self.profile)
 
     def addNodes(self, *anodes):
         for node in anodes:
@@ -105,3 +113,193 @@ class Model:
 
     def compose(self, profile):
         pass
+
+
+
+class ComponentManager:
+    def __init__(self):
+        self.components = []
+
+    def parseProfile(self, profile):
+        for i, v in enumerate(profile.data):
+            self.parse(v)
+
+    def parse(self, strList: list[str]):
+        if (len(strList) == 0):
+            return
+        ctype = strList[0]
+        match ctype:
+
+            case 'SYSBASE':
+                pass
+            case 'SYSFREQ':
+                pass
+            case 'THLINE':
+                self.addComponent(THLINE(strList))
+                pass
+            case 'LINE':
+                pass
+            case 'THTRFO':
+                self.addComponent(THTRFO(strList))
+                pass
+            case 'TRFO':
+                # TODO: add transformer
+                pass
+            case 'THFORB2':
+                pass
+            case 'THTRPH':
+                pass
+            case 'TAP':
+                pass
+            case 'TAPCV':
+                pass
+            case 'GENER':
+                self.addComponent(GENER(strList))
+                pass
+            case 'GENERCV':
+                self.addComponent(GENERCV(strList))
+                pass
+            case 'GENERDATA':
+                # gener=self.findComponentByName(strList[1])
+                # node=gener.node1
+                # node.Pmax=float(strList[2])
+                # node.Pmin=float(strList[3])
+                # node.Qmax=float(strList[4])
+                # node.Qmin=float(strList[5])
+                # node.Vmax=float(strList[6])
+                # node.Vmin=float(strList[7])
+                pass
+            case 'THLOAD':
+                self.addComponent(THLOAD(strList))
+                pass
+            case 'LOAD2':
+                # TODO: add load
+                pass
+            case 'THSLACK':
+                gener: GENER = self.findComponentByName(strList[1])
+                node: Node = gener.node1
+                node.changeType(NodeType.SLACK)
+                node.theta = 0
+                pass
+            case 'SLACKPH':
+                # TODO: add slack
+                pass
+            case 'THSHUNT':
+                self.addComponent(THSHUNT(strList))
+                pass
+
+    def addComponent(self, component):
+        self.components.append(component)
+
+    def findComponentByName(self, name):
+        for component in self.components:
+            if component.name == name:
+                return component
+        return None
+
+
+class THLINE(Component):
+    def __init__(self, strList):
+        self.type = strList[0]
+        self.name = strList[1]
+        self.node1 = strList[2]
+        self.node2 = strList[3]
+        self.R = float(strList[4])
+        self.X = float(strList[5])
+        self.nBf2 = float(strList[6])
+        print(f'Parsing {self.name}...')
+
+    def apply(self, model: Model):
+        node1: Node = model.findNodeByName(self.node1)
+        node2: Node = model.findNodeByName(self.node2)
+        branchLine = Branch(
+            self.name, node1, node2, Y=1/(self.R + self.X*1j))
+        node1.Ys += self.nBf2 * 1j
+        node2.Ys += self.nBf2 * 1j
+        model.addBranches(branchLine)
+
+
+class THTRFO(Component):
+    def __init__(self, strList):
+        self.type = strList[0]
+        self.name = strList[1]
+        self.node1 = strList[2]
+        self.node2 = strList[3]
+        self.R = float(strList[4])
+        self.X = float(strList[5])
+        self.k = float(strList[6])
+        print(f'Parsing {self.name}...')
+
+    def apply(self, model: Model):
+        node1: Node = model.findNodeByName(self.node1)
+        node2: Node = model.findNodeByName(self.node2)
+        branchT = Branch(
+            self.name, node1, node2, Y=1 / ((self.R + self.X*1j) / self.k))
+        node1.Ys += (self.k - 1) / (self.k * (self.R + self.X*1j))
+        node2.Ys += (1 - self.k) / (self.k**2 * (self.R + self.X*1j))
+        model.addBranches(branchT)
+
+
+class THSHUNT(Component):
+    def __init__(self, strList):
+        self.type = strList[0]
+        self.name = strList[1]
+        self.node1 = strList[2]
+        self.G = float(strList[3])
+        self.B = float(strList[4])
+        print(f'Parsing {self.name}...')
+
+    def apply(self, model: Model):
+        node1 = model.findNodeByName(self.node1)
+        node1.Ys += self.G + self.B * 1j
+
+
+class THLOAD(Component):
+    def __init__(self, strList):
+        self.type = strList[0]
+        self.name = strList[1]
+        self.node1 = strList[2]
+        self.P = float(strList[3])
+        self.Q = float(strList[4])
+        print(f'Parsing {self.name}...')
+
+    def apply(self, model: Model):
+        node1: Node = model.findNodeByName(self.node1)
+        node1.P += self.P
+        node1.Q += self.Q
+
+
+class GENERCV(Component):
+    def __init__(self, strList):
+        self.type = strList[0]
+        self.name = strList[1]
+        self.node1 = strList[2]
+        self.P = float(strList[3])
+        self.Q = float(strList[4])
+        self.V = float(strList[5])
+        self.state = int(strList[6])
+        print(f'Parsing {self.name}...')
+
+    def apply(self, model: Model):
+        node1: Node = model.findNodeByName(self.node1)
+        node1.P += self.P
+        node1.V = self.V
+        node1.changeType(NodeType.PV)
+
+
+class GENER(Component):
+    def __init__(self, strList):
+        self.type = strList[0]
+        self.name = strList[1]
+        self.node1 = strList[2]
+        self.P = float(strList[3])
+        self.Q = float(strList[4])
+        self.state = int(strList[5])
+        print(f'Parsing {self.name}...')
+
+    def apply(self, model: Model):
+        node1: Node = model.findNodeByName(self.node1)
+        node1.P += self.P
+        node1.Q += self.Q
+        node1.changeType(NodeType.PQ)
+
